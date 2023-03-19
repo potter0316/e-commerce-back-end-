6ba7deaa599a218f959250a26ec32ed2749d611b
potter0316 <117317671+potter0316@users.noreply.github.com>
2023-03-19 12:31:19 -0500

    additional routes for tag names

--- routes/api/tag-routes.js
+++ routes/api/tag-routes.js
@@ -25,7 +25,7 @@ router.get('/', async (req, res) => {
 });
 router.get('/:id', (req, res) => {
   // find a single tag by its `id`
-  const specificTag = Tag.findByPk(req.params.id, {
+  Tag.findByPk(req.params.id, {
     include: [
       {
         model: Product,
@@ -35,26 +35,38 @@ router.get('/:id', (req, res) => {
     ]
   })
     .then(retrievedTag => {
-      res.jsaon(retrievedTag);
+      res.json(retrievedTag);
   })
   // be sure to include its associated Product data
 });
 
 router.post('/', (req, res) => {
   // create a new tag
-});
-
-router.put('/:id', (req, res) => {
-  // update a tag's name by its `id` value
   Tag.create({
     tag_name: req.body.tag_name,
   })
     .then((tag) => {
-      res.jsaon(tag);
+      res.json(tag);
     })
     .catch((err) => {
-      res.jsaon(err);
+      res.json(err);
   })
+});
+
+router.put('/:id', (req, res) => {
+  // update a tag's name by its `id` value
+  Tag.update(
+    {
+      tag_name: req.body.tag_name,
+    },
+    {
+      where: {
+        id: req.params.id,
+      },
+    }
+  ).then((tag) => {
+    res.json(tag);
+        });
 });
 
 router.delete('/:id', (req, res) => {
